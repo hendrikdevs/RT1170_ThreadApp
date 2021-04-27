0d15fae32aea0425511dd9264597953c856d3a75
heisenbl <heisenbl@hs-mittweida.de>
2021-04-27 11:04:21 +0200

    Working example of threads (except polling)

--- src/threads.h
+++ src/threads.h
@@ -25,35 +25,16 @@ struct FifoCanMessageItem
  * @brief The main function for a worker thread.
  * 
  */
-void worker_function(void* arg1, void* arg2, void* arg3);
+void worker_thread_entry(void);
 
 /**
  * @brief The main function for a communication thread.
  * The communication thread is running in usermode.
  *
  */
-void communication_function(void* arg1, void* arg2, void* arg3);
+void communication_thread_entry(void);
 
-K_FIFO_DEFINE(communication_to_worker);
-K_FIFO_DEFINE(worker_to_communication);
-
-K_SEM_DEFINE(my_sem, 0, 1);
-
-struct k_poll_event events[2] = {
-    K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_SEM_AVAILABLE,
-                                    K_POLL_MODE_NOTIFY_ONLY,
-                                    &my_sem, 0),
-    K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_FIFO_DATA_AVAILABLE,
-                                    K_POLL_MODE_NOTIFY_ONLY,
-                                    &worker_to_communication, 0),
-};
-
-K_THREAD_STACK_DEFINE(communication_thread_stack, USER_STACKSIZE);
-// K_THREAD_STACK_DEFINE(worker_thread_stack, USER_STACKSIZE);
-K_KERNEL_STACK_DEFINE(worker_thread_stack, USER_STACKSIZE);
-
-K_THREAD_DEFINE(c1, USER_STACKSIZE, communication_function, NULL, NULL, NULL, COMMUNICATION_THREAD_PRIORITY, K_USER, 0);
-K_THREAD_DEFINE(w1, USER_STACKSIZE, worker_function, NULL, NULL, NULL,  WORKER_THREAD_PRIORITY, 0, 0);
-
+extern const k_tid_t c1;
+extern const k_tid_t w1;
 
 #endif /* THREADS_H */

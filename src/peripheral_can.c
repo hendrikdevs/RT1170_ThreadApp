1720501fb7a6c3ad5a84bfa90859bb91cfd5a1cd
heisenbl <heisenbl@hs-mittweida.de>
2021-05-04 10:59:42 +0200

    Logging and better comments

--- src/peripheral_can.c
+++ src/peripheral_can.c
@@ -4,7 +4,7 @@
 #include <device.h>
 #include <drivers/can.h>
 
-//LOG_MODULE_REGISTER(peripheral_can, CONFIG_PERIPHERAL_CAN_LOG_LEVEL);
+LOG_MODULE_REGISTER(peripheral_can);
 
 const struct zcan_filter can_standard_filter = {
         .id_type = CAN_STANDARD_IDENTIFIER,
@@ -24,14 +24,22 @@ int init_can(void)
 {
     int filter_id;
 
+    /* Get CAN Device */
     can_dev = device_get_binding(CAN_IDENTIFIER);
-    if(can_dev == NULL)
+    if(can_dev == NULL) {
+        LOG_ERR("No CAN device found");
         return CAN_UNITIALIZED;
-
-    filter_id = can_attach_isr(can_dev, receive, NULL, &can_standard_filter);
-    if (filter_id < 0) {
-        //LOG_ERR("Unable to attach can isr [%d]", filter_id);
     }
+
+    /* Attach ISR to can device */
+    filter_id = can_attach_isr(can_dev, receive, NULL, &can_standard_filter);
+    
+    /* Logging */
+    if (filter_id < 0) {
+        LOG_ERR("Unable to attach can isr [%d]", filter_id);
+    }
+
+    LOG_INF("CAN ISR attached and device intialized");
 
     return filter_id;
 }
@@ -45,7 +53,12 @@ int init_can(void)
  */
 void receive(struct zcan_frame *frame, void *arg)
 {
+    LOG_INF("CAN message received");
+
+    /* Reserve Space for FifoMessageItem on special heap for it */
     FifoMessageItem_t* work_item = k_heap_alloc(&message_item_heap, sizeof(FifoMessageItem_t), K_NO_WAIT);
+
+    /* Set Item Values based on received can values */
     work_item->dev = can_dev;
     work_item->send = send;
     work_item->message.priority = frame->data[0];
@@ -53,6 +66,7 @@ void receive(struct zcan_frame *frame, void *arg)
 
     strncpy(work_item->message.text, &frame->data[2], sizeof(work_item->message.text));
 
+    /* Send Item to communication thread */
     k_fifo_put(&extern_to_communication, work_item);
 }
 
@@ -76,7 +90,10 @@ void send(FifoMessageItem_t *item)
     int ret = -1;
     ret = can_send(item->dev, &send_frame, K_MSEC(100), NULL, NULL);
 
+    /* Log message status */
     if (ret != CAN_TX_OK) {
-        //LOG_ERR("Sending failed [%d]", ret);
+        LOG_ERR("CAN sending failed [%d]", ret);
     }
+
+    LOG_INF("CAN send success");
 }

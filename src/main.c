d390c15c0cdd150d03a64d39a7ab5f12b2178355
Jan Eilers <jeilers@hs-mittweida.de>
2021-05-10 14:30:55 +0200

    change printk to log

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ const k_timeout_t thread_timeout = K_FOREVER;
 
 void main(void) 
 {
-    printk("Hello World from main! %s\n", CONFIG_BOARD);
+    LOG_INF("Main thread started");
 
     #ifdef CONFIG_USE_DUMMY
     dummy_recieve_to_fifo();
@@ -21,20 +21,20 @@ void main(void)
 
     #ifdef CONFIG_USE_USB
     /* Intialize USB Peripheral */
-    int ret_usb;
+    int ret_usb = -1;
 
     ret_usb = init_usb();
-    if(ret_usb){
+    if(ret_usb == -1){
         LOG_ERR("Error while setting up USB");
     }
     #endif
 
     #ifdef CONFIG_USE_CAN
     /* Initialize CAN Peripheral */
-    int ret_can = -1;
+    int ret_can = CAN_UNITIALIZED;
     
     ret_can = init_can();
-    if(ret_can == -1) {
+    if(ret_can == CAN_UNITIALIZED) {
         LOG_ERR("Error while setting up CAN");
     }
     #endif
